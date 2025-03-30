b208b66ed46101ad040c2f39c99d924e0858964c
Michael Fatemi <myfatemi04@gmail.com>
2025-03-29 23:13:45 -0400

    Get the silence timeout to work

--- src/streaming.py
+++ src/streaming.py
@@ -1,5 +1,6 @@
 import asyncio
 from datetime import datetime
+from loguru import logger
 
 import websockets
 from openai import AsyncOpenAI
@@ -36,7 +37,7 @@ class Streaming:
         self.last_background_request_timestamp = datetime.now()
         self.openai_realtime_transcription_ws_ctx_manager = websockets.connect(
             OPENAI_WS_URL,
-            extra_headers={
+            additional_headers={
                 "Authorization": f"Bearer {openai_client.api_key}",
                 "OpenAI-Beta": "realtime=v1",
             },
@@ -47,7 +48,7 @@ class Streaming:
         self.openai_realtime_transcription_ws = (
             await self.openai_realtime_transcription_ws_ctx_manager.__aenter__()
         )
-        task1 = asyncio.create_task(self.process_audio_packets_loop())
+        task1 = asyncio.create_task(self.transcribe_loop())
         task2 = asyncio.create_task(self.generate_response_tokens_loop())
         task3 = asyncio.create_task(self.generate_speech_loop())
 
@@ -60,12 +61,16 @@ class Streaming:
         )
 
     async def on_audio_packet_received(self, packet_data: bytes):
+        logger.debug("Received audio packet")
         await self.audio_transcription_queue.put(packet_data)
 
-    async def on_transcription_result_received(self, result: str):
-        await self.transcribed_text_queue.put(result)
+    async def on_transcribed_text_received(self, text: str):
+        logger.debug("Received transcribed text: " + text)
+        await self.transcribed_text_queue.put(text)
 
-    async def process_audio_packets_loop(self):
+    async def transcribe_loop(self):
+        logger.info("Starting transcription loop")
+
         assert self.openai_realtime_transcription_ws
 
         while True:
@@ -78,7 +83,7 @@ class Streaming:
             # https://platform.openai.com/docs/guides/realtime-transcription#realtime-transcription-sessions
             result = await self.openai_realtime_transcription_ws.recv()
 
-            await self.transcribed_text_queue.put(result)
+            await self.on_transcribed_text_received(text=str(result))
 
     async def handle_tool_call(self, tool_call: dict):
         # Handles the tool call.
@@ -91,23 +96,33 @@ class Streaming:
             query = tool_call["arguments"]["query"]
 
     async def generate_response_tokens_loop(self):
+        logger.info("Starting response generation loop")
+
         while True:
             try:
                 # Dequeue all of the transcribed text and add it to the context.
-                while not self.transcribed_text_queue.empty():
+                run_once = False
+                while not run_once or not self.transcribed_text_queue.empty():
+                    run_once = True
                     transcribed_text_chunk = await asyncio.wait_for(
-                        self.transcribed_text_queue.get(),
-                        timeout=self.silence_period_s,
+                        self.transcribed_text_queue.get(), timeout=self.silence_period_s
                     )
                     self.context.add_text(
-                        transcribed_text_chunk, role="user", timestamp=datetime.now()
+                        transcribed_text_chunk["text"],
+                        role="user",
+                        timestamp=datetime.now(),
                     )
                     self.last_text_received_timestamp = datetime.now()
+
+                    logger.debug(
+                        "Added text to context: " + transcribed_text_chunk["text"]
+                    )
 
                 continue
             except asyncio.TimeoutError:
                 # If there was a timeout, it means that the silence period has been reached,
                 # and we are OK to send a request to OpenAI.
+                logger.debug("Silence period reached, sending request to OpenAI")
                 pass
 
             # Don't make another request if we haven't received any new text since the previous request.
@@ -129,6 +144,8 @@ class Streaming:
             # This is used for executing tool calls.
             event_loop = asyncio.get_event_loop()
             if background_request_period_passed and no_new_text_received:
+                logger.info("Performing `background` request")
+
                 # Do a "background" request.
                 self.last_background_request_timestamp = datetime.now()
                 async for delta in stream_openai_request_and_accumulate_toolcalls(
@@ -165,6 +182,8 @@ class Streaming:
                         # Interrupt the response if new text was received.
                         break
             else:
+                logger.info("Performing `user query` request")
+
                 # Do a "user query" request (handling the new text as if it's a user query).
                 self.last_user_query_request_timestamp = datetime.now()
                 async for delta in stream_openai_request_and_accumulate_toolcalls(
@@ -192,6 +211,8 @@ class Streaming:
                         break
 
     async def generate_speech_loop(self):
+        logger.info("Starting speech generation loop")
+
         while not self.tts_text_queue.empty():
             text = await self.tts_text_queue.get()
 
--- src/test_streaming.py
+++ src/test_streaming.py
@@ -1,9 +1,44 @@
+import asyncio
+import sys
+import wave
+
 import openai
+import dotenv
+
 from context import Context
 from streaming import Streaming
+import loguru
+
+dotenv.load_dotenv()
 
 
-openai_client = openai.AsyncOpenAI()
+async def main():
+    openai_client = openai.AsyncOpenAI()
 
-context = Context("./context", openai_client)
-streaming = Streaming(context, openai_client)
+    context = Context("./context", openai_client)
+    streaming = Streaming(context, openai_client)
+
+    run_task = asyncio.create_task(streaming.run())
+
+    wave_read = wave.Wave_read("test_files/test_file.wav")
+    framerate = wave_read.getframerate()
+
+    streaming.transcribed_text_queue.put_nowait(
+        {
+            "text": "Hello!",
+        }
+    )
+
+    # for _ in range(0, wave_read.getnframes(), chunk_size):
+    #     chunk = wave_read.readframes(chunk_size)
+    #     if not chunk:
+    #         break
+
+    #     await streaming.transcribed_text_queue(chunk)
+
+    await asyncio.gather(asyncio.sleep(15), run_task)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    print("Done")
